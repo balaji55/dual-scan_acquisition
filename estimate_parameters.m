eeaed25eb550d9fc3f1896f5727c464debb90e5f
jalan <jalanpalmer@gmail.com>
2012-05-20 16:09:25 -0400

    figure 3 and dependency are ready

---
+++ estimate_parameters.m
@@ -0,0 +1,104 @@
+function [theta_hat] = estimate_parameters(Gamma_min, Gamma_max, ...
+		number_of_scans, Y, B, initializations, B_m_guess)
+%
+% [theta_hat] = estimate_parameters(Gamma_min, Gamma_max, ...
+%     number_of_scans, Y, B, initializations, B_m_guess)
+%
+% This function estimates the unknown parameters from a set of scan data.
+%
+% The inputs are Gamma_min, Gamma_max, number_of_scans, Y, B, initializations,
+% and B_m_guess:
+%
+%     Gamma_min       : the minimum HWHM linewidth expected, in Gauss
+%     Gamma_max       : the maximum HWHM linewidth expected, in Gauss
+%     number_of_scans : how many different scans were performed
+%     Y               : the scan data, in arbitrary units
+%     B               : the field values for each data point
+%     initializations : how many initial guesses to try when fitting
+%     B_m_guess       : the modulation amplitude you set on the instrument
+%
+% The output is theta_hat = [d_hat Gamma_hat B_m_hat], the estimated parameter
+% values.
+%
+
+% Note: I am aware that number_of_scans is not strictly necessary, but it makes
+% for a nice sanity check.
+
+	% Argument checking
+	if ~isscalar(Gamma_min) || ~isfloat(Gamma_min) || ~(Gamma_min>0)
+		error('estimate_parameters:invalid_argument', ...
+			'Gamma_min must be a positive scalar float');
+	elseif ~isscalar(Gamma_max) || ~isfloat(Gamma_max) || ~(Gamma_max>0)
+		error('crlb_on_var:invalid_argument', ...
+			'Gamma must be a positive scalar float');
+	elseif ~isscalar(number_of_scans) || ~isfloat(number_of_scans) || ...
+		~(number_of_scans > 0)
+			error('estimate_parameters:invalid_argument', ...
+				'number_of_scans must be a positive scalar float');
+	elseif ~(size(Y, 1) == number_of_scans) || ~isfloat(Y)
+		error('estimate_parameters:invalid_argument', ...
+			'Y must be a number_of_scans-by-M matrix of floats');
+	elseif ~(size(B, 1) == number_of_scans) || ~isfloat(B)
+		error('estimate_parameters:invalid_argument', ...
+			'B must be a number_of_scans-by-M matrix of floats');
+	elseif ~isequal(size(Y), size(B))
+		error('estimate_parameters:invalid_argument', ...
+			'Y and B must have the same size');
+	elseif ~isscalar(initializations) || ~isfloat(initializations) || ...
+		~(initializations > 0)
+			error('estimate_parameters:invalid_argument', ...
+				'initializations must be a positive scalar float');
+	elseif ~isfloat(B_m_guess) || any(B_m_guess<0) || ...
+			~isequal(size(B_m_guess), [1 number_of_scans])
+		error('estimate_parameters:invalid_argument', ...
+		'B_m_guess must be a 1-by-number_of_scans vector of positive floats');
+	end
+
+	% We can try a few different initializations
+	Gamma_guess = linspace(Gamma_min, Gamma_max, initializations);
+
+	% Preallocate
+	theta_hats = zeros(initializations, number_of_scans + 2);
+	residual_norm = zeros(initializations, 1);
+
+	for i=1:initializations
+
+		guess = [0 Gamma_guess(i) B_m_guess];
+		[theta_hats(i,:), residual_norm(i)] = lsqnonlin(@fit_error, ...
+			guess,                         ... starting guess
+			zeros(1, number_of_scans + 2), ... lower bounds (zero)
+			inf(1, number_of_scans + 2),   ... upper bounds (infinity)
+			'',                            ... no special options
+			number_of_scans, Y, B);        ... extra arguments to fit_error
+	end
+
+	% Return the theta_hat with best residual norm
+	[~, index] = min(residual_norm);
+	theta_hat = theta_hats(index,:);
+end
+
+function [errors] = fit_error(theta, number_of_scans, Y, B)
+% theta = [d Gamma B_m]
+% d               : spin density, in arbitrary units
+% Gamma           : HWHM linewidth, in Gauss
+% B_m             : modulation amplitudes, in Gauss
+% number_of_scans : how many scans were performed
+% Y               : the scan data, in arbitrary units
+% B               : field values for each data point, in Gauss
+
+	% Break theta apart
+	d = theta(1);
+	Gamma = theta(2);
+	B_m = theta(3:end);
+
+	% Preallocate
+	errors = zeros(size(Y));
+
+	% Iterate over the scans
+	for i=1:1:number_of_scans
+		ideal_data = f([d Gamma B_m(i)], B(i,:));
+
+		errors(i,:) = Y(i,:) - ideal_data;
+	end
+
+end
---
+++ figure_3.m
@@ -0,0 +1,359 @@
+%% Display parameters for the plots
+font_size = 18;
+linewidth = 2;
+
+%% Scanning parameters
+M = 256;     % samples per scan
+d = 10;      % spin density, in arbitrary units
+sigma_N = 1; % noise standard deviation, in arbitrary units
+
+%% Simulation parameters
+simulations = 20;     % how many simulations per value of Gamma
+Gamma_steps = 10;     % how many values of Gamma to try across the range
+initializations = 10; % how many initial guesses to make at Gamma while fitting
+
+%% Range of linewidths suitable for tumor oximetry with a LiNc-BuO probe
+% These are HWHM linewidths, in Gauss
+tumor.Gamma_min = 0.356; %  0 mmHg O2
+tumor.Gamma_max = 0.460; % 15 mmHg O2
+tumor.Gamma = linspace(tumor.Gamma_min, tumor.Gamma_max, Gamma_steps);
+
+%% Scanning parameters for tumor oximetry, optimized single-scan
+tumor.single.B_m     = 1.61; % modulation amplitude, in Gauss
+tumor.single.Delta_B = 3.24; % sweep width, in Gauss
+tumor.single.scans   = 1;    % number of scans
+
+%% Predicted results for tumor oximetry, optimized single-scan
+tumor.single.predicted_std = zeros(size(tumor.Gamma));
+for i=1:length(tumor.Gamma)
+	tumor.single.predicted_std(i) = ...
+		sqrt(crlb_on_var( ...
+			[tumor.single.B_m; tumor.single.Delta_B], ...
+			tumor.single.scans, d, tumor.Gamma(i), sigma_N, M ...
+		));
+end
+
+%% Simulated results for tumor oximetry, optimized single-scan
+tumor.single.simulated_std = zeros(size(tumor.Gamma)); % preallocate
+for i=1:length(tumor.Gamma)
+
+	% Preallocate
+	Gamma_hat = zeros(1, simulations);
+	B = zeros(tumor.single.scans, M);
+	Y = zeros(tumor.single.scans, M);
+
+	for j=1:simulations
+
+		% Create noisy scans
+		for k=1:tumor.single.scans
+			B(k,:) = ...
+				linspace(-tumor.single.Delta_B(k)/2, ...
+					tumor.single.Delta_B(k)/2, M);
+			Y(k,:) = ...
+				f([d tumor.Gamma(i) tumor.single.B_m(k)], B(k,:)) + ...
+				sigma_N*sqrt(tumor.single.scans)*randn(1, M);
+		end
+
+		% Estimate parameters from the scans
+		theta_hat = ...
+			estimate_parameters(tumor.Gamma_min, tumor.Gamma_max, ...
+				tumor.single.scans, Y, B, initializations, tumor.single.B_m);
+		Gamma_hat(j) = theta_hat(2);
+	end
+
+	tumor.single.simulated_std(i) = std(Gamma_hat);
+end
+
+%% Scanning parameters for tumor oximetry, optimized dual-scan
+tumor.dual.B_m     = [0.65 3.17]; % modulation amplitudes, in Gauss
+tumor.dual.Delta_B = [1.38 4.68]; % sweep widths, in Gauss
+tumor.dual.scans   = 2;           % number of scans
+
+%% Predicted results for tumor oximetry, optimized dual-scan
+tumor.dual.predicted_std = zeros(size(tumor.Gamma));
+for i=1:length(tumor.Gamma)
+	tumor.dual.predicted_std(i) = ...
+		sqrt(crlb_on_var( ...
+			[tumor.dual.B_m; tumor.dual.Delta_B], ...
+			tumor.dual.scans, d, tumor.Gamma(i), sigma_N, M ...
+		));
+end
+
+%% Simulated results for tumor oximetry, optimized dual-scan
+tumor.dual.simulated_std = zeros(size(tumor.Gamma)); % preallocate
+for i=1:length(tumor.Gamma)
+
+	% Preallocate
+	Gamma_hat = zeros(1, simulations);
+	B = zeros(tumor.dual.scans, M);
+	Y = zeros(tumor.dual.scans, M);
+
+	for j=1:simulations
+
+		% Create noisy scans
+		for k=1:tumor.dual.scans
+			B(k,:) = ...
+				linspace(-tumor.dual.Delta_B(k)/2, ...
+					tumor.dual.Delta_B(k)/2, M);
+			Y(k,:) = ...
+				f([d tumor.Gamma(i) tumor.dual.B_m(k)], B(k,:)) + ...
+				sigma_N*sqrt(tumor.dual.scans)*randn(1, M);
+		end
+
+		% Estimate parameters from the scans
+		theta_hat = ...
+			estimate_parameters(tumor.Gamma_min, tumor.Gamma_max, ...
+				tumor.dual.scans, Y, B, initializations, tumor.dual.B_m);
+		Gamma_hat(j) = theta_hat(2);
+	end
+
+	tumor.dual.simulated_std(i) = std(Gamma_hat);
+end
+
+%% Average across the range
+tumor.single.predicted_mean_std = mean(tumor.single.predicted_std);
+tumor.single.simulated_mean_std = mean(tumor.single.simulated_std);
+tumor.dual.predicted_mean_std = mean(tumor.dual.predicted_std);
+tumor.dual.simulated_mean_std = mean(tumor.dual.simulated_std);
+
+%% Range of linewidths suitable for tissue oximetry with a LiNc-BuO probe
+% These are HWHM linewidths, in Gauss
+tissue.Gamma_min = 0.356; %  0 mmHg O2
+tissue.Gamma_max = 0.564; % 30 mmHg O2
+tissue.Gamma = linspace(tissue.Gamma_min, tissue.Gamma_max, Gamma_steps);
+
+%% Scanning parameters for tissue oximetry, optimized single-scan
+tissue.single.B_m     = 1.90; % modulation amplitude, in Gauss
+tissue.single.Delta_B = 3.84; % sweep width, in Gauss
+tissue.single.scans   = 1;    % number of scans
+
+%% Predicted results for tissue oximetry, optimized single-scan
+tissue.single.predicted_std = zeros(size(tissue.Gamma));
+for i=1:length(tissue.Gamma)
+	tissue.single.predicted_std(i) = ...
+		sqrt(crlb_on_var( ...
+			[tissue.single.B_m; tissue.single.Delta_B], ...
+			tissue.single.scans, d, tissue.Gamma(i), sigma_N, M ...
+		));
+end
+
+%% Simulated results for tissue oximetry, optimized single-scan
+tissue.single.simulated_std = zeros(size(tissue.Gamma)); % preallocate
+for i=1:length(tissue.Gamma)
+
+	% Preallocate
+	Gamma_hat = zeros(1, simulations);
+	B = zeros(tissue.single.scans, M);
+	Y = zeros(tissue.single.scans, M);
+
+	for j=1:simulations
+
+		% Create noisy scans
+		for k=1:tissue.single.scans
+			B(k,:) = ...
+				linspace(-tissue.single.Delta_B(k)/2, ...
+					tissue.single.Delta_B(k)/2, M);
+			Y(k,:) = ...
+				f([d tissue.Gamma(i) tissue.single.B_m(k)], B(k,:)) + ...
+				sigma_N*sqrt(tissue.single.scans)*randn(1, M);
+		end
+
+		% Estimate parameters from the scans
+		theta_hat = ...
+			estimate_parameters(tissue.Gamma_min, tissue.Gamma_max, ...
+				tissue.single.scans, Y, B, initializations, tissue.single.B_m);
+		Gamma_hat(j) = theta_hat(2);
+	end
+
+	tissue.single.simulated_std(i) = std(Gamma_hat);
+end
+
+%% Scanning parameters for tissue oximetry, optimized dual-scan
+tissue.dual.B_m     = [0.74 3.96]; % modulation amplitudes, in Gauss
+tissue.dual.Delta_B = [1.65 5.56]; % sweep widths, in Gauss
+tissue.dual.scans   = 2;           % number of scans
+
+%% Predicted results for tissue oximetry, optimized dual-scan
+tissue.dual.predicted_std = zeros(size(tissue.Gamma));
+for i=1:length(tissue.Gamma)
+	tissue.dual.predicted_std(i) = ...
+		sqrt(crlb_on_var( ...
+			[tissue.dual.B_m; tissue.dual.Delta_B], ...
+			tissue.dual.scans, d, tissue.Gamma(i), sigma_N, M ...
+		));
+end
+
+%% Simulated results for tissue oximetry, optimized dual-scan
+tissue.dual.simulated_std = zeros(size(tissue.Gamma)); % preallocate
+for i=1:length(tissue.Gamma)
+
+	% Preallocate
+	Gamma_hat = zeros(1, simulations);
+	B = zeros(tissue.dual.scans, M);
+	Y = zeros(tissue.dual.scans, M);
+
+	for j=1:simulations
+
+		% Create noisy scans
+		for k=1:tissue.dual.scans
+			B(k,:) = ...
+				linspace(-tissue.dual.Delta_B(k)/2, ...
+					tissue.dual.Delta_B(k)/2, M);
+			Y(k,:) = ...
+				f([d tissue.Gamma(i) tissue.dual.B_m(k)], B(k,:)) + ...
+				sigma_N*sqrt(tissue.dual.scans)*randn(1, M);
+		end
+
+		% Estimate parameters from the scans
+		theta_hat = ...
+			estimate_parameters(tissue.Gamma_min, tissue.Gamma_max, ...
+				tissue.dual.scans, Y, B, initializations, tissue.dual.B_m);
+		Gamma_hat(j) = theta_hat(2);
+	end
+
+	tissue.dual.simulated_std(i) = std(Gamma_hat);
+end
+
+%% Average across the range
+tissue.single.predicted_mean_std = mean(tissue.single.predicted_std);
+tissue.single.simulated_mean_std = mean(tissue.single.simulated_std);
+tissue.dual.predicted_mean_std = mean(tissue.dual.predicted_std);
+tissue.dual.simulated_mean_std = mean(tissue.dual.simulated_std);
+
+%% Range of linewidths suitable for broad-range oximetry with a LiNc-BuO probe
+% These are HWHM linewidths, in Gauss
+broad.Gamma_min = 0.356; %   0 mmHg O2
+broad.Gamma_max = 1.465; % 160 mmHg O2
+broad.Gamma = linspace(broad.Gamma_min, broad.Gamma_max, Gamma_steps);
+
+%% Scanning parameters for broad-range oximetry, optimized single-scan
+broad.single.B_m     = 4.36; % modulation amplitude, in Gauss
+broad.single.Delta_B = 8.95; % sweep width, in Gauss
+broad.single.scans   = 1;    % number of scans
+
+%% Predicted results for broad-range oximetry, optimized single-scan
+broad.single.predicted_std = zeros(size(broad.Gamma));
+for i=1:length(broad.Gamma)
+	broad.single.predicted_std(i) = ...
+		sqrt(crlb_on_var( ...
+			[broad.single.B_m; broad.single.Delta_B], ...
+			broad.single.scans, d, broad.Gamma(i), sigma_N, M ...
+		));
+end
+
+%% Simulated results for broad-range oximetry, optimized single-scan
+broad.single.simulated_std = zeros(size(broad.Gamma)); % preallocate
+for i=1:length(broad.Gamma)
+
+	% Preallocate
+	Gamma_hat = zeros(1, simulations);
+	B = zeros(broad.single.scans, M);
+	Y = zeros(broad.single.scans, M);
+
+	for j=1:simulations
+
+		% Create noisy scans
+		for k=1:broad.single.scans
+			B(k,:) = ...
+				linspace(-broad.single.Delta_B(k)/2, ...
+					broad.single.Delta_B(k)/2, M);
+			Y(k,:) = ...
+				f([d broad.Gamma(i) broad.single.B_m(k)], B(k,:)) + ...
+				sigma_N*sqrt(broad.single.scans)*randn(1, M);
+		end
+
+		% Estimate parameters from the scans
+		theta_hat = ...
+			estimate_parameters(broad.Gamma_min, broad.Gamma_max, ...
+				broad.single.scans, Y, B, initializations, broad.single.B_m);
+		Gamma_hat(j) = theta_hat(2);
+	end
+
+	broad.single.simulated_std(i) = std(Gamma_hat);
+end
+
+%% Scanning parameters for broad-range oximetry, optimized dual-scan
+broad.dual.B_m     = [1.78  9.50]; % modulation amplitudes, in Gauss
+broad.dual.Delta_B = [4.14 13.36]; % sweep widths, in Gauss
+broad.dual.scans   = 2;            % number of scans
+
+%% Predicted results for broad-range oximetry, optimized dual-scan
+broad.dual.predicted_std = zeros(size(broad.Gamma));
+for i=1:length(broad.Gamma)
+	broad.dual.predicted_std(i) = ...
+		sqrt(crlb_on_var( ...
+			[broad.dual.B_m; broad.dual.Delta_B], ...
+			broad.dual.scans, d, broad.Gamma(i), sigma_N, M ...
+		));
+end
+
+%% Simulated results for broad-range oximetry, optimized dual-scan
+broad.dual.simulated_std = zeros(size(broad.Gamma)); % preallocate
+for i=1:length(broad.Gamma)
+
+	% Preallocate
+	Gamma_hat = zeros(1, simulations);
+	B = zeros(broad.dual.scans, M);
+	Y = zeros(broad.dual.scans, M);
+
+	for j=1:simulations
+
+		% Create noisy scans
+		for k=1:broad.dual.scans
+			B(k,:) = ...
+				linspace(-broad.dual.Delta_B(k)/2, ...
+					broad.dual.Delta_B(k)/2, M);
+			Y(k,:) = ...
+				f([d broad.Gamma(i) broad.dual.B_m(k)], B(k,:)) + ...
+				sigma_N*sqrt(broad.dual.scans)*randn(1, M);
+		end
+
+		% Estimate parameters from the scans
+		theta_hat = ...
+			estimate_parameters(broad.Gamma_min, broad.Gamma_max, ...
+				broad.dual.scans, Y, B, initializations, broad.dual.B_m);
+		Gamma_hat(j) = theta_hat(2);
+	end
+
+	broad.dual.simulated_std(i) = std(Gamma_hat);
+end
+
+%% Average across the range
+broad.single.predicted_mean_std = mean(broad.single.predicted_std);
+broad.single.simulated_mean_std = mean(broad.single.simulated_std);
+broad.dual.predicted_mean_std = mean(broad.dual.predicted_std);
+broad.dual.simulated_mean_std = mean(broad.dual.simulated_std);
+
+%% New figure window
+figure();
+
+%% One plot with twelve bars
+bar([
+	tumor.single.predicted_mean_std  tumor.single.simulated_mean_std;
+	tumor.dual.predicted_mean_std    tumor.dual.simulated_mean_std;
+	tissue.single.predicted_mean_std tissue.single.simulated_mean_std;
+	tissue.dual.predicted_mean_std   tissue.dual.simulated_mean_std;
+	broad.single.predicted_mean_std  broad.single.simulated_mean_std;
+	broad.dual.predicted_mean_std    broad.dual.simulated_mean_std;
+]);
+set(gca(), 'Fontsize', font_size);
+set(gca(), 'Linewidth', linewidth);
+set(gca(), 'Xticklabel', {'Single' 'Dual' 'Single' 'Dual' 'Single' 'Dual'});
+xlim([0.5 6.5]);
+xlabel({'' 'Number of scans'});
+ylabel('Mean \Gamma std. (G)'); % add a hat above Gamma in post-processing
+legend('Predicted', 'Simulated', 'Location', 'Northwest');
+line([2.5 2.5], ylim(), ...
+	'Linewidth', linewidth, 'Linestyle', '-', 'Color', 'black');
+line([4.5 4.5], ylim(), ...
+	'Linewidth', linewidth, 'Linestyle', '-', 'Color', 'black');
+brighten(1); % brighten colors
+
+% This will need cleaned up in post-processing
+my_axis = axis();
+text(1, my_axis(4)*1.04, '0-15 mmHg', 'Fontsize', font_size);
+text(3, my_axis(4)*1.04, '0-30 mmHg', 'Fontsize', font_size);
+text(5, my_axis(4)*1.04, '0-160 mmHg', 'Fontsize', font_size);
+
+%% Save figure as an EPS file
+print('-depsc2', [mfilename() '.eps']);
